5dab80ae0d3b02fe7a23206395966562909e5ffa
vipz3 <vipz3@mail.ru>
2018-10-03 17:25:43 +0300

    Bug with adding undefined events is fixed

--- calendar/src/helpers/state.js
+++ calendar/src/helpers/state.js
@@ -17,22 +17,12 @@ export const getEventsState = (eventsState, year, month, dayInMonthNumber) => {
     }
 
 
-
-    // if ((eventsState[index] === undefined || eventsState[index][dayInMonthNumber] === undefined) && !("annualEvents" in resultEvents)) {
-    //     return null;
-    // } else {
-    //     console.log(resultEvents)
-    //     resultEvents["currentEvents"] = eventsState[index][dayInMonthNumber];
-    //     return resultEvents;
-    // }
-
     try {
         resultEvents["currentEvents"] = eventsState[index][dayInMonthNumber];
     } catch (e) {
-        console.log(e);
-    } finally {
 
-        if (Object.keys(resultEvents).length) {
+    } finally {
+        if (resultEvents["currentEvents"] !== undefined || resultEvents["annualEvents"] !== undefined) {
             return resultEvents;
         } else {
             return null;
