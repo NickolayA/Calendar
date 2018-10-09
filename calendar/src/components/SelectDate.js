575bad7b1341a9d29932318cc946aa0ca8c390b1
vipz3 <vipz3@mail.ru>
2018-10-09 10:16:40 +0300

    SelectDate.js component is fully redux

--- calendar/src/components/SelectDate.js
+++ calendar/src/components/SelectDate.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { connect } from "react-redux";
+import { changeDate } from "../actions/actions";
 
 const SelectDate = props => {
   return (
     <input
       type="month"
-      value={props.date.toISOString().substr(0, 7)}
-      onChange={props.onChangeDate}
+      value={props.currentDate.toISOString().substr(0, 7)}
+      onChange={e => props.changeDate(e)}
     />
   );
 };
 
-export default SelectDate;
+const mapStateToProps = state => {
+  return {
+    currentDate: state.date.currentDate
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    changeDate: e => {
+      const newDate = e.target.valueAsDate;
+      if (newDate) {
+        dispatch(changeDate(e.target.valueAsDate));
+      }
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SelectDate);
